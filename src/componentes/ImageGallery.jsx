5cbb8f449d1c131900338f85ff73ee385181600f
motorpsico97 <motorpsico1917@gmail.com>
2025-10-17 11:36:31 -0300

    mod nombre archivo

--- src/componentes/ImageGallery.jsx
+++ src/componentes/ImageGallery.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import '../../src/styles/imagegallery.css'
+import '../styles/gallery.css'
 
 
 const ImageGallery = ({ images = null, imagenes = null }) => {
